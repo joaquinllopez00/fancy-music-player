f3bf5714ab7b9ee98829f1b8a5c97283f6317f16
Joaquinllopez00 <joaquinllopezzz@gmail.com>
2021-03-08 14:10:43 -0500

    basic UI

---
+++ fancy-music-player/src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import "./styles/app.scss";
+import Player from "./components/Player";
+import Song from "./components/Song";
+import Library from "./components/Library";
+
+import data from "./util";
+function App() {
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+  return (
+    <div className="App">
+      <Song currentSong={currentSong} />
+      <Player setIsPlaying={setIsPlaying} isPlaying={isPlaying} currentSong={currentSong} />
+      <Library songs={songs} />
+    </div>
+  );
+}
+
+export default App;
